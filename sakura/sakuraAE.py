d5d76aaf1eceadc0b705023d8cc9a683d75e3f84
Vito Jiamin Chen <vitochan1@hotmail.com>
2025-01-28 16:22:47 +0800

    Update sakuraAE.py
    import typing test

--- sakura/sakuraAE.py
+++ sakura/sakuraAE.py
@@ -13,8 +13,9 @@ class InvalidKindError(Exception):
 def get_random_ingredients(kind=None):
     """
     Return a list of random ingredients as strings.
+    
     :param kind: Optional "kind" of ingredients.
-    :type kind: list[str] or None
+    :type kind: Optional[list[str]]
     :raise lumache.InvalidKindError: If the kind is invalid.
     :return: The ingredients list.
     :rtype: list[str]
